86677176bb7063debbc9b3237a78e814552d81a9
hungtamse <hungtamse@gmail.com>
2021-02-15 08:03:17 +0700

    add model and services

---
+++ common/services/users.service.ts
@@ -0,0 +1,42 @@
+import { CRUD } from '../interfaces/crud.interface';
+import { UserDto } from '../../users/dto/users.model';
+import UsersDao from '../../users/daos/users.dao';
+import usersDao from '../../users/daos/users.dao';
+
+class UsersService implements CRUD {
+    private static instance: UsersService;
+
+    static getInstance(): UsersService {
+        if (!UsersService.instance) {
+            UsersService.instance = new UsersService();
+        }
+        return UsersService.instance;
+    }
+
+    async list(limit: number, page: number): Promise<any>{
+        return await usersDao.getUsers();
+    };
+
+    async create(resource: any): Promise<any> {
+        return await usersDao.addUser(resource);
+    }
+
+    async updateById(resourceId: any): Promise<string> {
+        return await usersDao.putUserById(resourceId);
+    }
+
+    async readById(resourceId: any): Promise<any> {
+        return await usersDao.getUserById(resourceId);
+    }
+
+    async deleteById(resourceId: any): Promise<string> {
+        return await usersDao.removeUserById(resourceId);
+    }
+
+    async patchById(resourceId: any): Promise<string> {
+        return await usersDao.patchUserById(resourceId);
+    }
+    
+}
+
+export default UsersService.getInstance();
